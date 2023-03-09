bf0a33fee57bc53059e71668902040ad07b179ed
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-03-08 21:19:23 -0800

    Adds to dependencies and works on searchBooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBook, searchGoogleBooks } from '../utils/API'; //TODO: is this needed? or comment out?
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+//Use the Apollo useMutation () Hook to execute the SAVE_BOOK mutation in the handleSaveBook() function instead of the saveBook ( ) function imported from the API file.
+//Make sure you keep the logic for saving the book's ID to state in the try... catch block!
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -13,6 +18,9 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  //TODO: Should this go here?
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -29,7 +37,8 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      // const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -66,6 +75,8 @@ const SearchBooks = () => {
 
     try {
       const response = await saveBook(bookToSave, token);
+      //TODO:
+      // const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
