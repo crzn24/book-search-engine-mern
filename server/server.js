e72e0883bd12700ea0503ee83e8fbb97736beb5f
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-03-08 19:09:56 -0800

    Works on changing server from express to graphql

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+////TODO: Implement the Apollo Server and apply it to the Express server as middleware. ////
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes'); // Routes not needed
+// Import the ApolloServer class
+const { ApolloServer } = require("@apollo/server");
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const httpServer = http.createServer(app); //TODO: Do i need this?
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })], // TODO: do I need this?
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +28,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes); // not using express routes anymore so we will replace
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`
+  httpServer.listen(PORT, () => { //TODO: or should this still be app.listen?
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
 });
